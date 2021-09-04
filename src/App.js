6f41aba35d5e10dd6682e2d3288a670c1f47674c
mohsen.zergani <mohsen.zergani@gmail.com>
2021-09-04 18:54:03 +0430

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.scss";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/login/Login";
+import Register from "./Pages/register/Register";
+import Watch from "./Pages/watch/Watch";
+
 const App = () => {
   return (
     <div>
       <Home />
+      {/* <Login /> */}
+      {/* <Register /> */}
+      {/* <Watch /> */}
     </div>
   );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Featured from "../../Components/Featured/Featured";
+import List from "../../Components/List/List";
 import Navbar from "../../Components/Navbar/Navbar";
 import "./Home.scss";
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 };
